494d8efa18673b6c40e0317f9f8e9b9de2ef4eaa
jasminpomasoncco <u20201b236@upc.edu.pe>
2024-09-09 18:33:35 -0500

    feat: Add ArasaacService

--- apps/external_apis/arasaac.py
+++ apps/external_apis/arasaac.py
@@ -1,10 +1,28 @@
-
 import httpx
-from httpx import AsyncClient
 
-class ExternalApiArasaac:
-    async def search_berry():
-        url = 'https://pokeapi.co/api/v2/berry/cheri'
-        with httpx.Client() as client:
-            response = client.get(url)
+class ArasaacService:
+    BASE_URL = 'https://api.arasaac.org/v1/pictograms/es/'
+
+    @staticmethod
+    async def fetch_pictogram_data(pictogram_id):
+        url = f'{ArasaacService.BASE_URL}{pictogram_id}'
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url)
+            response.raise_for_status()
+            return response.json()
+
+    @staticmethod
+    async def fetch_pictogram_image(pictogram_id):
+        url = f'https://static.arasaac.org/pictograms/{pictogram_id}/{pictogram_id}_2500.png'
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url)
+            response.raise_for_status()
+            return response.url
+
+    @staticmethod
+    async def search_pictograms_by_word(word):
+        url = f'{ArasaacService.SEARCH_URL}search/{word}'
+        async with httpx.AsyncClient() as client:
+            response = await client.get(url)
+            response.raise_for_status()
             return response.json()
