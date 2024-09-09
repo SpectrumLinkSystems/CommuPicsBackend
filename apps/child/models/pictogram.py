d08963cc570e77203620004b2c341d307927bcfb
jasminpomasoncco <u20201b236@upc.edu.pe>
2024-09-09 17:13:48 -0500

    Refactor child model to include PictogramUsage relationship

--- apps/child/models/pictogram.py
+++ apps/child/models/pictogram.py
@@ -4,3 +4,12 @@ class Pictogram(models.Model):
     name = models.CharField(max_length=50)
     image_url = models.CharField(max_length=255)
     subcollection = models.ForeignKey("SubCollection", on_delete=models.CASCADE)
+
+class PictogramUsage(models.Model):
+    child = models.ForeignKey('Child', on_delete=models.CASCADE, related_name='pictogram_usages')
+    pictogram = models.ForeignKey('Pictogram', on_delete=models.CASCADE)
+    date_used = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f'{self.child.name} - {self.pictogram.name} - {self.date_used}'
--- apps/urls.py
+++ apps/urls.py
@@ -17,14 +17,31 @@ Including another URLconf
 from django.urls import path
 from django.http import HttpResponse
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from dotenv import load_dotenv, dotenv_values
 import os
 
 from apps.recognition.views import RecognitionView
 
 load_dotenv()
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Mi API",
+        default_version='v1',
+        description="Descripción de mi API",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@myapi.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
-    path('hello/', lambda request: HttpResponse(os.getenv("DB_NAME"))),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/parents/', include('apps.parents.urls')),
     path('api/therapists/', include('apps.therapists.urls')),
     path('api/', include('apps.child.urls')),
