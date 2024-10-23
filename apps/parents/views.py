b2ed2c9154d7b43a54fee38737a4ccae78bc04dc
LenninHuaman <u20201b975@upc.edu.pe>
2024-10-23 02:07:14 -0500

    fix: Changed return to void to Parent in get_parent_by_firebase_id

--- apps/parents/views.py
+++ apps/parents/views.py
@@ -1,3 +1,5 @@
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import OpenApiParameter, extend_schema
 from rest_framework import status, viewsets
 from rest_framework.decorators import action, api_view
 from rest_framework.response import Response
@@ -18,7 +20,7 @@ class ParentViewSet(viewsets.ModelViewSet):
         if parent:
             serializer = self.get_serializer(parent)
             return Response(
-                {"message": "Parent created", "parent": serializer.data},
+                serializer.data,
                 status=status.HTTP_201_CREATED,
             )
         return Response(
@@ -30,9 +32,19 @@ class ParentViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(parents, many=True)
         return Response(serializer.data)
 
-    @api_view(["GET"])
-    def get_queryset(self, *args, **kwargs):
-        firebase_id = self.request.query_params.get("firebase_id")
+    @extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "firebase_id",
+                OpenApiTypes.STR,
+                OpenApiParameter.QUERY,
+            )
+        ]
+    )
+    @action(detail=False, methods=["get"])
+    def get_parent_by_firebase_id(self, request):
+        firebase_id = request.query_params.get("firebase_id", None)
+        print(firebase_id)
         if not firebase_id:
             return Response(
                 {"error": "firebase_id parameter is required"},
@@ -41,7 +53,8 @@ class ParentViewSet(viewsets.ModelViewSet):
 
         parent = get_parent_by_firebase_id(firebase_id)
         if parent:
-            return Response(status=status.HTTP_200_OK)
+            serializer = self.get_serializer(parent)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response({"error": "Parent not found"}, status=status.HTTP_404_NOT_FOUND)
 
     def retrieve(self, request, *args, **kwargs):
