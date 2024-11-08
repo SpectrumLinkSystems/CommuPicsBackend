3b3ce4be0d1eb710611f4e8e3a9870195396daf7
jasminpomasoncco <u20201b236@upc.edu.pe>
2024-11-07 22:00:33 -0500

    🐞 fix: Improve logic for pictogram recommendation selection and clean up code

--- apps/recomendation/recomendation_service.py
+++ apps/recomendation/recomendation_service.py
@@ -6,6 +6,7 @@ from apps.child.models.pictogram import Pictogram, PictogramUsage
 from datetime import datetime
 
 def get_pictogram_recommendations(child_id, selected_pictogram_id):
+
     selected_pictogram = Pictogram.objects.get(id=selected_pictogram_id)
     
     pictograms = Pictogram.objects.all()
@@ -23,7 +24,6 @@ def get_pictogram_recommendations(child_id, selected_pictogram_id):
             'cant_used': usage.cant_used,
             'pictogram': pictogram
         })
-
         used_categories.extend(pictogram.arasaac_categories.split(", "))
     
     used_pictograms_df = pd.DataFrame(used_pictograms)
@@ -44,10 +44,14 @@ def get_pictogram_recommendations(child_id, selected_pictogram_id):
 
     vectorizer = TfidfVectorizer()
     tfidf_matrix = vectorizer.fit_transform([pic['arasaac_categories'] for pic in relevant_pictograms])
-    
-    similarity_matrix = cosine_similarity(tfidf_matrix)
 
-    selected_idx = relevant_pictograms.index(next(pic for pic in relevant_pictograms if pic['pictogram'] == selected_pictogram))
+    similarity_matrix = cosine_similarity(tfidf_matrix)
+    
+    selected_idx = next((i for i, pic in enumerate(relevant_pictograms) if pic['pictogram'] == selected_pictogram), None)
+
+    if selected_idx is None:
+
+        selected_idx = 0
 
     sim_scores = list(enumerate(similarity_matrix[selected_idx]))
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
@@ -57,7 +61,6 @@ def get_pictogram_recommendations(child_id, selected_pictogram_id):
         similar_pictograms.append(relevant_pictograms[i[0]])
 
     popular_pictograms = sorted(similar_pictograms, key=lambda x: x['cant_used'], reverse=True)
-    print("POPULARIDAD",popular_pictograms)
     
     recommendations = []
     for pic in popular_pictograms[:3]:
